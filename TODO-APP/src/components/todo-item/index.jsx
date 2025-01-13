bd0eaee4b8dae57b257a52d5bd564f7d105bc8aa
priyadarshnni <priyadarshniii15@gmail.com>
2025-01-14 00:54:40 +0530

    Second Commit

---
+++ TODO-APP/src/components/todo-item/index.jsx
@@ -0,0 +1,45 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
+function TodoItem({ todo, fetchDetailsOfCurrentTodo }) {
+  console.log(todo);
+  return (
+    <Card
+      sx={{
+        maxWidth: 350,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      <CardContent>
+        <Typography variant="h5" color={"text.secondary"}>
+          {todo?.todo}
+        </Typography>
+      </CardContent>{" "}
+      <CardActions>
+        <Button
+          onClick={() => fetchDetailsOfCurrentTodo(todo?.id)}
+          sx={{
+            backgroundColor: "lightpink",
+            color: "#hotpink",
+            opacity: "0.75",
+            "&:hover": {
+              backgroundColor: "#000000",
+              color: "#fff",
+              opacity: "1",
+            },
+          }}
+        >
+          Details
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default TodoItem;
